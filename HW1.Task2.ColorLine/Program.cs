23bd3f72aefd141939fe56066d6baa8b90be29a3
Mik <Mik@MIKAEL>
2023-03-27 10:55:18 +0300

    Add project files.

---
+++ HW1.Task2.ColorLine/Program.cs
@@ -0,0 +1,84 @@
+﻿int size = 30;
+int[,] matrix = new int[size, size];
+
+Random rand = new Random();
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        matrix[i, j] = rand.Next(0, 16);
+    }
+}
+
+int maxLength = 0;
+int startRow = -1;
+int startCol = -1;
+int endRow = -1;
+int endCol = -1;
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    int currentLength = 1;
+    int currentStartCol = 0;
+
+    for (int j = 1; j < matrix.GetLength(1); j++)
+    {
+        if (matrix[i, j] == matrix[i, j - 1])
+        {
+            currentLength++;
+        }
+        else
+        {
+            if (currentLength > maxLength)
+            {
+                maxLength = currentLength;
+                startRow = i;
+                startCol = currentStartCol;
+                endRow = i;
+                endCol = j - 1;
+            }
+
+            currentLength = 1;
+            currentStartCol = j;
+        }
+    }
+
+    if (currentLength > maxLength)
+    {
+        maxLength = currentLength;
+        startRow = i;
+        startCol = currentStartCol;
+        endRow = i;
+        endCol = matrix.GetLength(1) - 1;
+    }
+}
+
+if (maxLength > 0)
+{
+    Console.WriteLine("Color: " + matrix[startRow, startCol] + " | Start: (" + startRow + ", " + startCol + ") | End: (" + endRow + ", " + endCol + ") | Length: " + maxLength);
+}
+else
+{
+    Console.WriteLine("No horizontal lines found.");
+}
+
+
+for (int i = 0; i < size; i++)
+{
+    for (int j = 0; j < size; j++)
+    {
+        if (matrix[i, j] < 10)
+        {
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), matrix[i, j].ToString());
+            Console.Write(matrix[i, j] + "  ");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), matrix[i, j].ToString());
+            Console.Write(matrix[i, j] + " ");
+            Console.ResetColor();
+        }
+    }
+    Console.WriteLine();
+}
---
+++ HW1_Task1_SnakeMatrix/Program.cs
@@ -0,0 +1,94 @@
+﻿Console.WriteLine("Enter row count:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter col count:");
+int n = int.Parse(Console.ReadLine());
+
+int counter1 = 1;
+int[,] matrix1 = new int[m, n];
+for (int i = 0; i < m; i++)
+{
+    matrix1[i, 0] = counter1;
+    counter1++;
+}
+for (int i = 1; i < n; i++)
+{
+    matrix1[m - 1, i] = counter1;
+    counter1++;
+}
+for (int i = m - 2; i >= 0; i--)
+{
+    matrix1[i, n - 1] = counter1;
+    counter1++;
+}
+for (int i = n - 2; i >= 1; i--)
+{
+    matrix1[0, i] = counter1;
+    counter1++;
+}
+int c = 1;
+int d = 1;
+
+while (counter1 < m * n)
+{
+
+    while (matrix1[c + 1, d] == 0)
+    {
+        matrix1[c, d] = counter1;
+        counter1++;
+        c++;
+    }
+
+    while (matrix1[c, d + 1] == 0)
+    {
+        matrix1[c, d] = counter1;
+        counter1++;
+        d++;
+    }
+
+    while (matrix1[c - 1, d] == 0)
+    {
+        matrix1[c, d] = counter1;
+        counter1++;
+        c--;
+    }
+
+    while (matrix1[c, d - 1] == 0)
+    {
+        matrix1[c, d] = counter1;
+        counter1++;
+        d--;
+    }
+
+}
+
+//При данном решении в центре всегда остаётся незаполненная ячейка.
+//Убираем её при помощи следующего цикла.
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (matrix1[i, j] == 0)
+        {
+            matrix1[i, j] = counter1;
+        }
+    }
+}
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (matrix1[i, j] < 10)
+        {
+            Console.Write(matrix1[i, j] + "  ");
+        }
+        else
+        {
+            Console.Write(matrix1[i, j] + " ");
+        }
+    }
+    Console.WriteLine();
+}
+
+
+
