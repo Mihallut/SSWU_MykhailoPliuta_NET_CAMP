290f151bf488837264d74fe84f38a886537bbeb0
Михаил Плюта <mikhailpliuta@gmail.com>
2023-04-03 14:53:17 +0300

    Initial

---
+++ Home_Task_3/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialText1 = "word word";
+            int? index = TextWorker.FindSecondEntranceIndex(initialText1, "word");
+            Console.WriteLine(index);
+
+            string initialText2 = "word Word word word Word";
+            int count = TextWorker.CapitalizedCount(initialText2);
+            Console.WriteLine(count);
+
+            string initialText3 = "word Worrd wword wordd Word";
+            string result = TextWorker.ReplaceWordsWithDoubling(initialText3, "WORD");
+            Console.WriteLine(result);
+        }
+    }
+}
