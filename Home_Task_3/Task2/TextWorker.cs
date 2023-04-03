6430f76391c635d09025e18b95bcaa0410916b83
Михаил Плюта <mikhailpliuta@gmail.com>
2023-04-03 14:53:17 +0300

    Initial

---
+++ Home_Task_3/Task2/TextWorker.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Task2
+{
+    static internal class TextWorker
+    {
+        public static int? FindSecondEntranceIndex(string text, string soughtFor)
+        {
+
+            if (text.Length > soughtFor.Length)
+            {
+                if (text.Contains(soughtFor))
+                {
+                    int firstIndex = text.IndexOf(soughtFor);
+                    if (firstIndex == -1)
+                    {
+                        return null;
+                    }
+                    int secondIndex = text.IndexOf(soughtFor, firstIndex + soughtFor.Length);
+                    if (secondIndex == -1)
+                    {
+                        return null;
+                    }
+                    return secondIndex;
+                }
+            }
+            return null;
+        }
+
+        public static int CapitalizedCount(string text)
+        {
+
+            int count = 0;
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (char.IsUpper(words[i][0]))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+
+        public static string ReplaceWordsWithDoubling(string text, string substitute)
+        {
+            string[] words = text.Split(' ');
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (ContainDouble(words[i]))
+                {
+                    words[i] = substitute;
+                }
+            }
+            string result = String.Join(' ', words);
+
+            return result;
+        }
+
+        private static bool ContainDouble(string word)
+        {
+            word = word.ToLower();
+            for (int i = 0; i < word.Length - 1; i++)
+            {
+                if (word[i] == word[i + 1])
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
