6714647c4c23cec9f6fcbe30a75b8bc961a5b2d0
Михаил Плюта <mikhailpliuta@gmail.com>
2023-04-28 17:13:17 +0300

    Initial

---
+++ Home_Task_6/Task_3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "word word1 word2 word1 word2 word3";
+            List<string> uniqueWords = TextWorker.GetUniqueWords(text);
+
+            foreach (string word in uniqueWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
---
+++ Home_Task_6/Task_3/TextWorker.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3
+{
+    public static class TextWorker
+    {
+
+        public static List<string> GetUniqueWords(string text)
+        {
+            return ProcessText(text).ToList();
+        }
+        private static IEnumerable<string> ProcessText(string text)
+        {
+            string[] words = text.Split(new[] { ' ', '.', ',', ';', ':', '!', '?', '-', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            foreach (string word in words)
+            {
+                if (IsUniqueWord(word, words))
+                {
+                    yield return word;
+                }
+            }
+        }
+        private static bool IsUniqueWord(string word, string[] words)
+        {
+            int count = 0;
+            foreach (string w in words)
+            {
+                if (w.ToLower() == word.ToLower())
+                {
+                    count++;
+                }
+            }
+            return count == 1;
+        }
+    }
+}
