bc312dc88db92affbce989e93747a497f0ddb1ef
Михаил Плюта <mikhailpliuta@gmail.com>
2023-04-28 14:46:27 +0300

    Initial

---
+++ Home_Task_6/Task_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[,] matrix = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+
+            SnakeMatrix snakeMatrix = new SnakeMatrix(matrix);
+            foreach (int i in snakeMatrix)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
---
+++ Home_Task_6/Task_1/SnakeMatrix.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    public class SnakeMatrix : IEnumerable
+    {
+        private int[,] _matrix;
+
+        public SnakeMatrix(int[,] matrix)
+        {
+            _matrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+            Array.Copy(matrix, _matrix, matrix.Length);
+            Console.WriteLine();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int rowCount = _matrix.GetLength(0);
+            int colCount = _matrix.GetLength(1);
+            for (int i = 0; i < rowCount + colCount - 1; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    for (int row = Math.Min(i, rowCount - 1), col = i - row; row >= 0 && col < colCount; row--, col++)
+                    {
+                        yield return _matrix[row, col];
+                    }
+                }
+                else
+                {
+                    for (int col = Math.Min(i, colCount - 1), row = i - col; col >= 0 && row < rowCount; col--, row++)
+                    {
+                        yield return _matrix[row, col];
+                    }
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
