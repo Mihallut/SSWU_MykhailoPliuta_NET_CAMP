83d82f2737fb7f27110fbf3efc08967cbe4f9da2
Михаил Плюта <mikhailpliuta@gmail.com>
2023-04-28 16:44:35 +0300

    Initial

---
+++ Home_Task_6/Task_2/Merger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    public static class Merger
+    {
+        public static int[] Merge(params int[][] nums)
+        {
+            return GetSortedArray(nums).ToArray();
+        }
+
+        private static IEnumerable<int> GetSortedArray(params int[][] arrays)
+        {
+            var mergedArray = arrays.SelectMany(x => x).OrderBy(x => x);
+            foreach (int item in mergedArray)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ Home_Task_6/Task_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 5, 1, 3 };
+            int[] arr2 = { 8, 2, 3, 4 };
+
+            int[] result = Merger.Merge(arr1, arr2);
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
