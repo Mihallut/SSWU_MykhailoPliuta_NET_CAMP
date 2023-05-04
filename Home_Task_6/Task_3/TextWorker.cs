fc53462d9d00d40cf17eba4a34cbdde6fc243371
senivlm <91743906+senivlm@users.noreply.github.com>
2023-05-04 13:24:33 +0300

    Update TextWorker.cs

--- Home_Task_6/Task_3/TextWorker.cs
+++ Home_Task_6/Task_3/TextWorker.cs
@@ -5,7 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Task_3
-{
+{// сумарний бал 92
+    
     public static class TextWorker
     {
 
@@ -27,7 +28,7 @@ namespace Task_3
             }
         }
         private static bool IsUniqueWord(string word, string[] words)
-        {
+        {// можна перервати цикл при cont=1.також можна було скористатись стандартним методом Count()
             int count = 0;
             foreach (string w in words)
             {
